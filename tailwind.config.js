4afc30d8f76b49c2e82f359cbce647e26279c5e1
Lily Moon <53998902+BoringLily@users.noreply.github.com>
2023-04-16 18:18:45 -0400

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/client/**/*.{js,ts,jsx,tsx}",
+    "./src/client/index.css"
+  ],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'black': '#000000',
+      'lilac': '#CFB8FF',
+      'grey': {
+        100: '#FBFBFB',
+        200: '#F0F0F0',
+        400: '#D9D9D9',
+        500: '#B8B8B8',
+        600: '#8A8A8A',
+        700: '#5F5F5F',
+        800: '#060606'
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Nunito', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}

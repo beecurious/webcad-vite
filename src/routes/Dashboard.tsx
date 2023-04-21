c56cef741ef93101c9c436624f5dc241e7a8d5b9
Amanda Coleman <acoleman8097@floridapoly.edu>
2023-04-21 15:28:12 -0400

    Favorites Display on Dashboard by defult NOT connected to API

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -9,11 +9,29 @@ export default function Dashboard() {
     
 
     return (
+   <>   
+    <div className="w-96">
+    </div>
 
-    <div className="">
-        
-    </div> 
-         
-         
+   <div className=" ">
+    <h1 className=" text-2xl  mt-8 "> Favorites:</h1>
+   <div className=" min-w-full flex flex-row  gap-2 m-9">
+   <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
+    <p>Favorite #1</p>
+    <p>Last opened: 3 hours ago</p>
+   </div>
+   <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
+    <p>Favorite #1</p>
+    <p>Last opened: 3 hours ago</p>
+   </div> <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
+    <p>Favorite #1</p>
+    <p>Last opened: 3 hours ago</p>
+   </div>
+   </div>
+    </div>
+   
+   
+   </>
+      
 )
 }

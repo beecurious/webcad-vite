9994ba1ecaf3e593a125760660d25aafed4fa297
Amanda Coleman <acoleman8097@floridapoly.edu>
2023-04-22 10:26:44 -0400

    Defult Dashboard view not connected to API

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -10,25 +10,47 @@ export default function Dashboard() {
 
     return (
    <>   
-    <div className="w-96">
+    <div className="w-36"></div>
+        <h1 className=" text-2xl  mt-8 "> Recent:  </h1>
+    <div>
+        
+    <div className=" min-w-full flex flex-row gap-2 mt-32">
+        <div className='Favorite box-border  border-grey-700 rounded-lg h-auto w-42 p-4 border-2 '> 
+            <img
+                src="src/assets/Rectangle 9.png"
+                alt="PlaceHolder"
+            ></img>
+            <p>Project #1</p>
+            <p>Last opened: 3 hours ago</p>
+        </div>
+        <div className='Favorite box-border  border-grey-700 rounded-lg h-auto w-42 p-4 border-2 '> 
+            <img
+                src="src/assets/Rectangle 9.png"
+                alt="PlaceHolder"
+            ></img>
+            <p>Project #2</p>
+            <p>Last opened: 8 hours ago</p>
+        </div>
+         <div className='Favorite box-border  border-grey-700 rounded-lg h-auto w-42 p-4 border-2 '> 
+            <img
+                src="src/assets/Rectangle 9.png"
+                alt="PlaceHolder"
+            ></img>
+            <p>Project #3</p>
+            <p>Last opened: 16 hours ago</p>
+        </div>
     </div>
+    <div className='Favorite box-border  border-grey-700 rounded-lg h-auto  max-w-sm p-4 border-2 '> 
+            <img
+                src="src/assets/Rectangle 9.png"
+                alt="PlaceHolder"
+            ></img>
+            <p>Project #3</p>
+            <p>Last opened: 16 hours ago</p>
+        </div>
+    </div>
+    
 
-   <div className=" ">
-    <h1 className=" text-2xl  mt-8 "> Favorites:</h1>
-   <div className=" min-w-full flex flex-row  gap-2 m-9">
-   <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
-    <p>Favorite #1</p>
-    <p>Last opened: 3 hours ago</p>
-   </div>
-   <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
-    <p>Favorite #1</p>
-    <p>Last opened: 3 hours ago</p>
-   </div> <div className='Favorite box-border  border-grey-700 rounded-lg h-32 w-42 p-4 border-2 '> 
-    <p>Favorite #1</p>
-    <p>Last opened: 3 hours ago</p>
-   </div>
-   </div>
-    </div>
    
    
    </>
